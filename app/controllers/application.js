b405d5433a0214effac91a22a03a2b9f39b70db0
Alex Todoran <alex.todoran@luminos.software>
2020-01-06 15:25:59 +0200

    darkmode enabled for currentUser

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,24 +1,15 @@
 import Controller from '@ember/controller';
-import { computed } from "@ember/object";
-
 
 export default Controller.extend({
-  currentUser: computed(function() {
-    return this.get('store').findRecord('user', 42)
-  }),
-
-  hasDarkMode: computed('currentUser.darkMode', function() {
-    return this.get('currentUser.darkMode');
-  }),
-
-  test: true,
-
   init(){  
     this._super(...arguments);
-    if (this.get('hasDarkMode')) { 
-      document.body.classList.add("darkMode");
-    } else {
-      document.body.classList.remove("darkMode");
-    }
+    this.get('store').findRecord('user', 42).then(currentUser => {
+      if (currentUser.darkMode) {
+        this.set('darkMode', currentUser.darkMode);
+        document.body.classList.add("darkMode");
+      } else {
+        document.body.classList.remove("darkMode");
+      }
+     });
   }
 });
