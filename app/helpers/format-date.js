7cd55fb09a0ae6edb7d950481c32f36e5b55a7f7
Alex Todoran <alex.todoran@luminos.software>
2020-01-09 11:31:53 +0200

    pretty date format

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,6 +1,7 @@
 import { helper } from '@ember/component/helper';
 
 export default helper(function formatDate(params/*, hash*/) {
-  return params.moment().format('LL');
+  return moment().format('LL');
 });
+
 
