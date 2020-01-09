acd23eb6b8901f62cfbfa2a09d8978213f25940a
Alex Todoran <alex.todoran@luminos.software>
2020-01-09 11:08:11 +0200

    current user progress

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -8,18 +8,20 @@ export default Controller.extend({
 
   init(){  
     this._super(...arguments);
-    this.get('store').findRecord('user', 42).then(currentUser => {
-      if (currentUser.darkMode) {
-        this.set('darkMode', currentUser.darkMode);
+    let id=42;
+    this.get('store').findRecord('user', id).then(item => {
+      if (item.darkMode) {
+        this.set('darkMode', item.darkMode);
         document.body.classList.add("darkMode");
       } else {
         document.body.classList.remove("darkMode");
       }
+      this.set('currentUser', item);
      });
   },
   actions: {
-    changeUser: function() {
-      this.set('currentUser', currentUser)
+    changeUser: function(currentUserVariable) {
+      this.set('currentUser', currentUserVariable);
     },
   }
 });
